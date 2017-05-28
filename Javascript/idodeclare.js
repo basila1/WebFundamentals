37628c32af2f3d9f39812c1312b4e198b4135582
basila1 <basila_n@outlook.com>
2017-05-27 20:41:38 -0700

    practice

---
+++ Javascript/idodeclare.js
@@ -0,0 +1,21 @@
+var fav_number= 6;
+var cats= 2;
+var dances= 5;
+var num= 10;
+var my_name= "Basila";
+var fav_food= "thai food";
+var location= "mountain view";
+var ilikedancing= true;
+var ihatedancing= false;
+var x;
+
+console.log("my favorite number is " + fav_number);
+console.log("I have " + cats+" cats");
+console.log("How many times have I perfomed? "+ dances);
+console.log("I have gone on "+ num + " trips");
+console.log("My name is "+ my_name);
+console.log("My favorite foos is "+ fav_food);
+console.log("I live in "+ location);
+console.log("I like to dance, "+ ilikedancing);
+console.log("I don't like to dance, "+ ihatedancing);
+console.log(x)
